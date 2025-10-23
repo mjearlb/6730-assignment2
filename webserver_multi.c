6b1ea2b6de4477847dbb6c21495eb90d26955017
Milo Jefferson Bauman <mjb01161@uga.edu>
2025-10-23 09:43:02 -0400

    Added thread monitor and changed buffer pointer

--- webserver_multi.c
+++ webserver_multi.c
@@ -4,6 +4,8 @@
 #include <pthread.h>
 #include "webserver.h"
 #include <semaphore.h>
+#include <signal.h>
+#include<unistd.h>
 
 #define MAX_REQUEST 100
 
@@ -13,6 +15,8 @@ pthread_mutex_t mutex;
 sem_t full;
 sem_t empty;
 int bufLoc;
+int in;
+int out;
 
 void *worker() {
 	// Worker loop
@@ -21,8 +25,8 @@ void *worker() {
 		pthread_mutex_lock(&mutex);     // Lock the mutex
 
 		/* DO SOMETHING */
-		int fd = buffer[bufLoc];        // Get the fd from the buffer
-		bufLoc = (bufLoc - 1) % MAX_REQUEST; // Move the buffer index back 1
+		int fd = buffer[out];        // Get the fd from the buffer
+		out = (out + 1) % MAX_REQUEST; // Move the buffer index back 1
 
 		pthread_mutex_unlock(&mutex);   // Unlock the mutex
 		sem_post(&empty);               // Signal there is a new empty space in buffer
@@ -66,8 +70,8 @@ void *listener() {
 		sem_wait(&empty);               // Wait for there to be an empty spot in the buffer
 		pthread_mutex_lock(&mutex);     // Lock the mutex
 
-		bufLoc = (bufLoc + 1) % MAX_REQUEST; // Move the buffer index forward 1
-		buffer[bufLoc] = s;             // Place fd into buffer
+		buffer[in] = s;                 // Place fd into buffer
+		in = (in + 1) % MAX_REQUEST;    // Move the buffer index forward 1
 
 		pthread_mutex_unlock(&mutex);   // Unlock the mutex
 		sem_post(&full);                // Signal that there is a new full space in the buffer
@@ -75,6 +79,31 @@ void *listener() {
 
 	close(sock);
 } // listener
+
+void threadMonitor(pthread_t workers[], pthread_t *listenerThread) {
+	while (1) {
+		// Check each of the worker threads
+		for (int i = 0; i < numThread; i++) {
+			if (pthread_kill(workers[i], 0) != 0) {
+				printf("Error: Worker thread %i died! Restarting thread %i...\n", i+1, i+1);
+				while (pthread_create(&workers[i], NULL, worker, NULL) != 0) {
+					printf("Error restarting thread %i, trying again...\n", i+1);
+				} // while
+			} // if
+		} // for
+
+		// Check the listener thread
+		if (pthread_kill(*listenerThread, 0) != 0) {
+			printf("Error: Listener thread died! Restarting thread...\n");
+			while (pthread_create(listenerThread, NULL, listener, NULL) != 0) {
+				printf("Error restarting listener thread, trying again...\n");
+			} // while
+		} // if
+
+		// Sleep
+		sleep(1);
+	} // while
+} // threadMonitor
 
 void thread_control() {
 
@@ -84,7 +113,8 @@ void thread_control() {
 	sem_init(&empty, 0, MAX_REQUEST);    // number of empty spaces in buffer (MAX_REQUEST)
 
 	// Set buffer pointer
-	bufLoc = MAX_REQUEST;                          // Slot that will be edited in the buffer
+	in = 0;
+	out = 0;
 
 	// Create 1 listener thread
 	pthread_t listenerThread;
@@ -92,7 +122,7 @@ void thread_control() {
 		printf("Error: Failed to create listener thread.");
 		exit(1);
 	} // if
-	printf("Listener thread created and ready.\n");
+	// printf("Listener thread created and ready.\n");
 
 	// Create numThreads worker threads
 	pthread_t workerThread[numThread];
@@ -101,17 +131,15 @@ void thread_control() {
 			printf("Error: Failed to create worker thread %i.\n", i);
 			exit(1);
 		} // if
-		printf("Worker thread %i created and ready to process requests.\n", i+1);
+		// printf("Worker thread %i created and ready to process requests.\n", i+1);
 	} // for
 
-	// Wait for the threads to terminate
-	for (int i = 0; i < numThread; i++) {
-		pthread_join(workerThread[i], NULL); // Worker threads
-	} // for
-	pthread_join(listenerThread, NULL);      // Listener thread
+	// Monitor the threads (BROKEN)
+	threadMonitor(workerThread, &listenerThread);
 
 	// TODO: Destroy the semaphores(?)
 } // thread_control
+
 
 int main(int argc, char *argv[]) {
 	if(argc != 3 || atoi(argv[1]) < 2000 || atoi(argv[1]) > 50000) {
